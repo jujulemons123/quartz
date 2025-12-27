fb5c0b874df5a84afabf6f0f258dd801c458582f
Julia Willemyns <juliawillemyns@Julias-MacBook-Air.local>
2025-12-27 14:43:29 +0000

    Quartz sync: Dec 27, 2025, 2:43 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -40,11 +40,6 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.Explorer(),
   ],
-  right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)

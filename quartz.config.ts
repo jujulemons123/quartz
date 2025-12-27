83f981e1ec8d4e9a18465b9b9e518049e4ed466e
Julia Willemyns <juliawillemyns@Julias-MacBook-Air.local>
2025-12-27 22:17:09 +0000

    Quartz sync: Dec 27, 2025, 10:17 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,49 +1,80 @@
-import { PageLayout, SharedLayout } from "./quartz/cfg"
-import * as Component from "./quartz/components"
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
 
-export const sharedPageComponents: SharedLayout = {
-  head: Component.Head(),
-  header: [],
-  afterBody: [], 
-  footer: Component.Footer({ links: {} }),
+// THIS LINE WAS MISSING OR BROKEN. IT DEFINES "config":
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "JGW's Archive",
+    enableGPA: false,
+    enablePageViews: false,
+    defaultDateType: "created",
+    generateSocialImages: false,
+    baseUrl: "jujulemons123.github.io/quartz",
+    ignorePatterns: [],
+    enablePopovers: true, // Turns on popovers
+    theme: {
+      fontOrigin: "googleFonts",
+      cdnCaching: true,
+      typography: {
+        header: "Cormorant Garamond",
+        body: "Lustria",
+        code: "IBM Plex Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#FFFDE7",
+          lightgray: "#C5C2A5",
+          gray: "#979690",
+          darkgray: "#4A3728",
+          dark: "#1B2620",
+          secondary: "#8B0000",
+          tertiary: "#225C59",
+          highlight: "rgba(139, 0, 0, 0.05)",
+          textHighlight: "#fff23688",
+        },
+        darkMode: {
+          light: "#161618",
+          lightgray: "#393639",
+          gray: "#646464",
+          darkgray: "#d4d4d4",
+          dark: "#ebebec",
+          secondary: "#7b97aa",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#b3aa0288",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.CreatedModifiedDate({ priority: ["frontmatter", "filesystem"] }),
+      Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.SyntaxHighlighting(),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.TableOfContents(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Description(),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources(),
+      Plugin.ContentPage(),
+      Plugin.FolderPage({
+        showFolderTitle: false,
+        showFolderCount: false,
+      }),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({ enableSiteMap: true, enableRSS: true }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.NotFoundPage(),
+    ],
+  },
 }
 
-export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-    Component.ConditionalRender({
-      component: Component.Breadcrumbs(),
-      condition: (page) => page.fileData.slug !== "index",
-    }),
-    Component.ArticleTitle(),
-    Component.TagList(),
-  ],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    // Standard "Official" Stack - No overlapping possible
-    Component.Search(),
-    Component.Darkmode(),
-    Component.ReaderMode(),
-  ],
-  right: [
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
-}
-
-export const defaultListPageLayout: PageLayout = {
-  beforeBody: [
-    Component.Breadcrumbs(), 
-    Component.ArticleTitle(), 
-  ],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
-  ],
-  right: [],
-}
-
-
+// THIS LINE EXPORTS THE CONFIG DEFINED ABOVE
 export default config
